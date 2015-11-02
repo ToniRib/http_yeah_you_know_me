a62b90e2602ccd2d1dbf6680405ca1fcff501a2b
ToniRib <tonimarierib@gmail.com>
2015-11-02 16:51:10 -0700

    refactor: Move splitting of request to all separate methods

--- lib/app.rb
+++ lib/app.rb
@@ -1,19 +1,46 @@
+require 'pry'
+
 class App
   def hello_world(i)
     "Hello, World (#{i})"
   end
 
   def convert_request_to_html(request)
-    verb, path, protocol = request.first.split
-    host, port = request[1].split[1].split(":")
-    accept = request[4]
-    data = ["Verb: #{verb}", "Path: #{path}", "Protocol: #{protocol}",
-     "Host: #{host}", "Port: #{port}", "Origin: #{host}",
-     accept]
+    data = ["Verb: #{verb(request)}", "Path: #{path(request)}",
+            "Protocol: #{protocol(request)}", "Host: #{host(request)}",
+            "Port: #{port(request)}", "Origin: #{host(request)}", "#{accept(request)}"]
+    create_html_string(data)
+  end
+
+  def create_html_string(data)
     "<pre>\n#{data.join("\n")}\n</pre>"
   end
 
   def generate_response(i, request)
     hello_world(i) + "\n" + convert_request_to_html(request)
+  end
+
+  def verb(request)
+    request.first.split[0]
+  end
+
+  def path(request)
+    request.first.split[1]
+  end
+
+  def protocol(request)
+    request.first.split[2]
+  end
+
+  def host(request)
+    request[1].split[1].split(":")[0]
+  end
+
+  def port(request)
+    request[1].split[1].split(":")[1]
+  end
+
+  def accept(request)
+    request[4]
   end
 end
