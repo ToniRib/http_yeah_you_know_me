a92e9b33bb46f208391d495c759f578fa7b061c8
ToniRib <tonimarierib@gmail.com>
2015-11-02 16:34:12 -0700

    feat: Combines hello_world response and html

--- lib/app.rb
+++ lib/app.rb
@@ -4,15 +4,16 @@ class App
   end
 
   def convert_request_to_html(request)
-    verb = request.first.split[0]
-    path = request.first.split[1]
-    protocol = request.first.split[2]
-    host = request[1].split[1].split(":").first
-    port = request[1].split[1].split(":").last
+    verb, path, protocol = request.first.split
+    host, port = request[1].split[1].split(":")
     accept = request[4]
     data = ["Verb: #{verb}", "Path: #{path}", "Protocol: #{protocol}",
      "Host: #{host}", "Port: #{port}", "Origin: #{host}",
      accept]
     "<pre>#{data.join("\n")}</pre>"
+  end
+
+  def generate_response(i, request)
+    hello_world(i) + "\n" + convert_request_to_html(request)
   end
 end
--- spec/app_test.rb
+++ spec/app_test.rb
@@ -13,6 +13,12 @@ class AppTest < Minitest::Test
     assert_equal expected, @app.hello_world(0)
   end
 
+  def test_generates_hello_world_with_different_response
+    expected = "Hello, World (1)"
+
+    assert_equal expected, @app.hello_world(1)
+  end
+
   def test_generates_html_from_request
     request = ["GET / HTTP/1.1",
                "Host: 127.0.0.1:9292",
@@ -25,15 +31,39 @@ class AppTest < Minitest::Test
                "Accept-Language: en-US,en;q=0.8"]
 
     request_data = ["Verb: GET",
-      "Path: /",
-      "Protocol: HTTP/1.1",
-      "Host: 127.0.0.1",
-      "Port: 9292",
-      "Origin: 127.0.0.1",
-      "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"]
+                    "Path: /",
+                    "Protocol: HTTP/1.1",
+                    "Host: 127.0.0.1",
+                    "Port: 9292",
+                    "Origin: 127.0.0.1",
+                    "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"]
 
     expected = "<pre>#{request_data.join("\n")}</pre>"
 
     assert_equal expected, @app.convert_request_to_html(request)
+  end
+
+  def test_generates_hello_world_response_with_diagnostics
+    request = ["GET / HTTP/1.1",
+               "Host: 127.0.0.1:9292",
+               "Connection:keep-alive",
+               "Cache-Control: max-age=0",
+               "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+               "Upgrade-Insecure-Requests: 1",
+               "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36",
+               "Accept-Encoding: gzip, deflate, sdch",
+               "Accept-Language: en-US,en;q=0.8"]
+
+    request_data = ["Verb: GET",
+                    "Path: /",
+                    "Protocol: HTTP/1.1",
+                    "Host: 127.0.0.1",
+                    "Port: 9292",
+                    "Origin: 127.0.0.1",
+                    "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"]
+
+    expected = "Hello, World (0)\n<pre>#{request_data.join("\n")}</pre>"
+
+    assert_equal expected, @app.generate_response(0, request)
   end
 end
