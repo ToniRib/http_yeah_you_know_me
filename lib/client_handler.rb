7b5b7273788a12f2744d09dee858665bc2144a70
ToniRib <tonimarierib@gmail.com>
2015-11-02 15:32:08 -0700

    feat: Client Handler works with indexing hello world by one

--- lib/client_handler.rb
+++ lib/client_handler.rb
@@ -1,15 +1,16 @@
 require 'socket'
+require 'pry'
 
 class ClientHandler
   attr_reader :server, :client
 
   def initialize(port = 9292)
-    @server = TCPServer.new(port)
+    @server = TCPServer.open(port)
   end
 
 
   def get_request
-    client = server.accept
+    @client = server.accept
 
     request_lines = []
     while line = client.gets and !line.chomp.empty?
@@ -19,20 +20,20 @@ class ClientHandler
     request_lines
   end
 
-  # def post_response(i)
-  #   client = server.accept
-  #
-  #   client.puts "Hello, World! (#{i})"
-  # end
+  def post_response(i)
+    client.puts "Hello, World! (#{i})"
+    client.close
+  end
 end
 
 ch = ClientHandler.new
 # machine = Machine.new
 i = 0
+
 loop do
   request = ch.get_request
-  puts request
-#   response = ch.machine.generate_response(request)
-  # ch.post_response(i)
-  i += 1
+  # puts request
+  # response = ch.machine.generate_response(request)
+  ch.post_response(i)
+  i += 1 if request.first.include?("favicon")
 end
