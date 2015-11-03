8915d3b86e59b031bc7f01ad5893f3d1f14b9e33
ToniRib <tonimarierib@gmail.com>
2015-11-03 16:29:04 -0700

    tests: Adds tests for Responses class

--- spec/responses_test.rb
+++ spec/responses_test.rb
@@ -6,19 +6,39 @@ class ResponsesTest < Minitest::Test
     @responses = Responses.new
   end
 
-  def test_generates_hello_world_zero_response
+  def test_generates_hello_world_response
     expected = "Hello, World (0)"
 
     assert_equal expected, @responses.hello_world(0)
   end
 
-  def test_generates_hello_world_with_different_response
+  def test_generates_hello_world_different_response
     expected = "Hello, World (1)"
 
     assert_equal expected, @responses.hello_world(1)
   end
 
-  #more tests
+  def test_generates_good_luck_response
+    assert_equal 'Good luck!', @responses.good_luck
+  end
+
+  def test_generates_empty_string_for_root_response
+    assert_equal '', @responses.root
+  end
+
+  def test_generates_shutdown_response
+    assert_equal 'Total Requests: 1', @responses.shutdown(1)
+  end
+
+  def test_generates_different_shutdown_response
+    assert_equal 'Total Requests: 5', @responses.shutdown(5)
+  end
+
+  def test_generates_datetime_response
+    today = Time.now.strftime('%l:%M%p on %A, %B %e, %Y')
+
+    assert_equal today, @responses.datetime
+  end
 
   def test_responds_with_known_word_if_word_is_pizza
     expected = "pizza is a known word"
